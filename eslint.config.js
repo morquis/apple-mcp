e49c3aee2f06dbd0705c9e488c6b1d8b92c6be3b
morquis <48450508+morquis@users.noreply.github.com>
2025-06-04 16:56:27 +0200

    chore: normalize indentation and update scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,17 @@
-import js from "@eslint/js";
-import ts from "@typescript-eslint/eslint-plugin";
-import parser from "@typescript-eslint/parser";
+import ts from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
 
 export default [
-  js.configs.recommended,
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     languageOptions: {
       parser,
       parserOptions: {
-        project: "./tsconfig.json",
-        sourceType: "module",
+        project: './tsconfig.json',
+        sourceType: 'module',
       },
     },
-    plugins: { "@typescript-eslint": ts },
-    rules: {
-    },
+    plugins: { '@typescript-eslint': ts },
+    rules: {},
   },
 ];
